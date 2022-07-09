f15cbca08daf721a3132b6724d2ab82b3a928cba
Eldon Ahrold <eahrold>
2022-07-09 17:46:33 +0000

    Initial commit

---
+++ src/test/java/com/example/session/HandoffTest.java
@@ -0,0 +1,21 @@
+package com.example.session;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class HandoffTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
